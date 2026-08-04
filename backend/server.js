8c4581bfa492431f96828e54a87e857d90a2bc8c
ivanishvilil774-dot <ivanishvilil774@gmail.com>
2026-08-04 23:03:08 +0400

    cors is fixed

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,10 @@ const projectRouter = require('./routes/project.routes')
 
 const app = express()
 
-const CLIENT_URL = process.env.CLIENT_URL || "https://goa-academy-luka.netlify.app/";
+const CLIENT_URL = process.env.CLIENT_URL || "https://goa-academy-luka.netlify.app";
 
 app.use(cors({
-    origin: CLIENT_URL,
+    origin: true,
     credentials: true,
 }));
 
